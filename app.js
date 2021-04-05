945bc8c60f24cf8cb28e386c16f92c1d103c86f2
kiffykowboy <chris@Chriss-MacBook-Pro.local>
2021-04-05 17:18:26 -0500

    4.5

--- app.js
+++ app.js
@@ -22,21 +22,41 @@ const logTodos = () => {
   console.log(arrayOfTodos)
 }
 
+// const populateTodos = () => {
+//   const ol = document.getElementById('todo-list');
+
+//   for (let index = 0; index < arrayOfTodos.length; index++) { 
+//     const li = document.createElement("li")
+//     const title = arrayOfTodos[index].title
+//     const listItemText = document.createTextNode(title);
+//     li.appendChild(listItemText)
+//     document.body.insertBefore(li, ol)
+  
+//   }
+
+
+
+
+
 const populateTodos = () => {
-  const ol = document.getElementById('todo-list');
-
-  for (let index = 0; index < arrayOfTodos.length; index++) { 
-    const li = document.createElement("li")
-
-    const title = arrayOfTodos[index].title
-
-    const listItemText = document.createTextNode(title);
-
-    li.appendChild(listItemText)
-
-    document.body.insertBefore(li, ol)
-
-    // const element = arrayOfTodos[index];
-    // console.log('element:', element)
+  for(i=0; i < (arrayOfTodos.length)/10; i++) {
+  const list = document.getElementById('todo-list')
+  const listItem = document.createElement('li')
+  const task = document.createTextNode(arrayOfTodos[i].title)
+  listItem.appendChild(task)
+  list.appendChild(listItem)
+  if (arrayOfTodos[i].completed === false) {
+    listItem.style.color = "red"
+  } else {
+    listItem.style.color = "green"
+    listItem.style.textDecoration = "line-through"
   }
   }
+  }
+
+  const clearTodos = () => {
+    console.log("!!!!!")
+    document.getElementById('todo-list').innerHTML = ""
+  }
+
+  
